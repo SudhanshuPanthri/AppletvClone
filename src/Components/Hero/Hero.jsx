c73e812acfc8e2d8a06eeaf913ba75a4defafa6a
Sudhanshu Panthri <panthrisudhanshu666@gmail.com>
2021-08-10 12:20:13 +0530

    Banner Img

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
-import './Hero.css'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, { useEffect, useState } from "react";
+import "./Hero.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import axios from "../../axios";
+import requests from "../../Requests";
 
 function Hero() {
-    return (
-        <div className='hero'>
-            <div className="content">
-                <h1>All Apple Originals.</h1>
-                <h2>Exclusively on Apple TV+. Watch here and on the Apple TV app across your devices.</h2>
-                <button className='btn'>Start your free trial</button>
-                <span>7 days free, then ₹ 99/month</span>
-                <ExpandMoreIcon style={{
-                    fontSize: 32,
-                    cursor: 'pointer'
-                }} />
-            </div>
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTopRated);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div
+        className="hero"
+        style={{
+          backgroundImage: `
+            url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        }}
+      >
+        <div className="content">
+          <h1>All Apple Originals.</h1>
+          <h2>
+            Exclusively on Apple TV+. Watch here and on the Apple TV app across
+            your devices.
+          </h2>
+          <button className="btn">Start your free trial</button>
+          <span>7 days free, then ₹ 99/month</span>
+          <ExpandMoreIcon
+            style={{
+              fontSize: 32,
+              cursor: "pointer",
+            }}
+          />
         </div>
-    )
+      </div>
+      <div className="contentCards"></div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
